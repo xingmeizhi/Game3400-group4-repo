f05240734fb387a7362035002706dc94548b0d08
alyssachen04 <alyssachen04@gmail.com>
2024-02-08 15:13:58 -0500

    Finished Player controller

--- Game3400/Assets/PlayerController.cs
+++ Game3400/Assets/PlayerController.cs
@@ -8,8 +8,10 @@ public class BasicPlayerController : MonoBehaviour
     Create a variable called 'rb' that will represent the 
     rigid body of this object.
     */
+    public GameObject ratKing;
+    Vector3 ratPosition;
     private Rigidbody rb;
-    public float moveSpeed = 2;
+    public float moveSpeed = 0;
     /* 
     Creates a public variable that will be used to set 
     the movement intensity (from within Unity)
@@ -19,11 +21,17 @@ public class BasicPlayerController : MonoBehaviour
     {
     	// make our rb variable equal the rigid body component
         rb = GetComponent<Rigidbody>();
+        ratPosition = ratKing.transform.position;
+        moveSpeed = 0;
         // yPos = transform.position.y;
     }
  
     void Update()
     {
+        ratPosition = ratKing.transform.position;
+        if (Vector3.Distance(ratPosition, transform.position) < 3) {
+            moveSpeed = 2;
+        }
         // Move Forwards
         if (Input.GetKey(KeyCode.W)) 
         {
